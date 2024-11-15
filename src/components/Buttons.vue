cae831c4f4476e79c47b656f7792aa29489c11c3
RamonLGDaw <ramonlg.daw@gmail.com>
2024-11-15 21:09:52 +0100

    prueba actualización

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container-fluid d-flex flex-column justify-content-center align-items-center mx-0 p-5 bg-light">
       <h2>Funcionarà???</h2>
+      <p>Petición fetch</p>
       <div class="container d-flex justify-content-center align-items-center m-auto mt-4 ">
         <button @click="enviarEstado('ON')" type="submit" class="btn btn-success m-3 fuente">ON</button>
         <button @click="enviarEstado('OFF')" type="submit" class="btn btn-danger m-3 fuente">OFF</button>
